f590739b5d3bac12f84790241e6280b3e726c555
Finn Ickler <finnickler@gmail.com>
2018-06-14 11:51:11 +0100

    Made BarChart more fancy

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -1,8 +1,10 @@
 // Originally from https://medium.com/@Elijah_Meeks/interactive-applications-with-react-d3-f76f7b3ebc71
 import React, { Component } from 'react'
-import { scaleLinear } from 'd3-scale'
+import { scaleLinear, scaleBand } from 'd3-scale'
 import { max } from 'd3-array'
 import { select } from 'd3-selection'
+import {axisBottom, axisLeft} from  'd3-axis'
+import './BarChart.css'
 class BarChart extends Component {
   constructor(props){
     super(props);
@@ -15,35 +17,52 @@ class BarChart extends Component {
     this.createBarChart()
   }
   createBarChart() {
-    const node = this.node;
-    const dataMax = max(this.props.data);
-    const yScale = scaleLinear()
-        .domain([0, dataMax])
-        .range([0, this.props.size[1]]);
-    select(node)
-        .selectAll('rect')
-        .data(this.props.data)
-        .enter()
-        .append('rect');
+    const {
+      data,width,height,fill,x,y,barWidth,margin
+    } = this.props;
 
-    select(node)
-        .selectAll('rect')
-        .data(this.props.data)
-        .exit()
+    const node = this.node;
+    const xScale = scaleBand().rangeRound([0, width]).padding(0.1)
+        .domain(data.map(x));
+    const yScale = scaleLinear().rangeRound([height, 0])
+        .domain([0, max(data, y)]);
+
+    // This is here so it doesn't render it on top of each other every time it is updated
+    select(node).selectAll('*')
         .remove();
 
-    select(node)
-        .selectAll('rect')
-        .data(this.props.data)
-        .style('fill', '#fe9922')
-        .attr('x', (d,i) => i * 25)
-        .attr('y', d => this.props.size[1] - yScale(d))
-        .attr('height', d => yScale(d))
-        .attr('width', 25)
+    const g = select(node)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(axisBottom(xScale));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(axisLeft(yScale).ticks(10, "%").tickPadding(10))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Frequency");
+
+    g.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr('x', d => xScale(x(d)))
+        .attr('y', d => yScale(y(d)))
+        .attr('width', xScale.bandwidth())
+        .attr('height', d => height - yScale(y(d)));
   }
   render() {
     return <svg ref={node => this.node = node}
-                width={this.props.size[0]} height={this.props.size[1]}/>
+                width={this.props.width + this.props.margin.right + this.props.margin.left}
+                height={this.props.height + this.props.margin.top + this.props.margin.bottom}/>
   }
 }
 export default BarChart
--- client/src/pages/Page5.js
+++ client/src/pages/Page5.js
@@ -4,26 +4,45 @@ import BarChart from '../components/BarChart'
 
 const chartProperties = {
   title: "Chosen Options",
-  data: [10,20,30,40,10],
-  width: 500,
-  height: 500,
-  chartSeries : [
-    {
-      field: 'Percent',
-      name: 'Frequency'
-    }
-  ],
-  x : d => d.Label,
+  data: [{Letter: "A" ,Frequency: .08167},
+         {Letter: "B" ,Frequency: .01492},
+         {Letter: "C" ,Frequency: .02782},
+         {Letter: "D" ,Frequency: .04253},
+         {Letter: "E" ,Frequency: .12702}],
+  width: 900,
+  height: 450,
+  barWidth : 25,
+  x : (d) => d.Letter,
+  y : (d) => d.Frequency,
   xScale : 'ordinal',
   xLabel : "Letter",
   yLabel : "Frequency",
   yTicks : [10, "%"],
+  fill : "black",
+  margin : {top: 20, right: 20, bottom: 30, left: 40}
 };
 
 function Page5 (props) {
   return (
     <div name ='page5' className='flexFixedSize flexContainerColumn fullPage'>
-      <BarChart data={chartProperties.data} size={[chartProperties.width,chartProperties.height]} />
+      <BarChart
+          fill={chartProperties.fill}
+          title= {chartProperties.title}
+          data= {chartProperties.data}
+          width= {chartProperties.width}
+          margin = {chartProperties.margin}
+          height= {chartProperties.height}
+          chartSeries = {chartProperties.chartSeries}
+          x= {chartProperties.x}
+          y= {chartProperties.y}
+          xLabel= {chartProperties.xLabel}
+          xScale= {chartProperties.xScale}
+          yTicks= {chartProperties.yTicks}
+          yLabel = {chartProperties.yLabel}
+          barWidth = {chartProperties.barWidth}/>
+      {/*<BarChart data={chartProperties.data}*/}
+                {/*size={[chartProperties.width,chartProperties.height]*/}
+                {/*} />*/}
     </div>
   );
 }
