f3d7f05b46705049e1e7a928c82dd5761c86d676
Finn Ickler <finnickler@gmail.com>
2018-06-28 00:21:17 +0100

    Deleted the old icons and replaced them in page 6

--- client/src/pages/Page6.js
+++ client/src/pages/Page6.js
@@ -5,47 +5,47 @@ const datasourceProperties = [
   {
     actionDescription: 'Do 10,000 steps per day',
     actionShows: 'You\'re active and healthy',
-    iconSrc: '/fitbit.png',
+    iconSrc: '/logos/fitbit.png',
   },
   {
     actionDescription: 'Visit a lot of points of interest',
     actionShows: 'You\'re familiar with the area',
-    iconSrc: '/fitbit.png',
+    iconSrc: '/logos/fitbit.png',
   },
   {
     actionDescription: 'Get 8 hours sleep a night',
     actionShows: 'You\'re well rested and have energy',
-    iconSrc: '/fitbit.png',
+    iconSrc: '/logos/fitbit.png',
   },
   {
     actionDescription: 'Liked 20 videos and disliked 53 videos',
     actionShows: 'You\'re more likely to share negative views',
-    iconSrc: '/youtube.png',
+    iconSrc: '/logos/youtube.png',
   },
   {
     actionDescription: 'Subscribe to Music, News and Food channels',
     actionShows: 'You have a broad range of interests',
-    iconSrc: '/youtube.png',
+    iconSrc: '/logos/youtube.png',
   },
   {
     actionDescription: 'Comment on 3 of every 10 videos you watch',
     actionShows: 'Your\'re comfortable sharing your opinion',
-    iconSrc: '/youtube.png',
+    iconSrc: '/logos/youtube.png',
   },
   {
     actionDescription: 'Started books on History, Art and Bibliographies',
     actionShows: 'You have a broad range of interests',
-    iconSrc: '/amazon.png',
+    iconSrc: '/logos/amazon.png',
   },
   {
     actionDescription: 'Only buy items on your saved list at the end of the month',
     actionShows: 'You\'re not an impulsive spender',
-    iconSrc: '/amazon.png',
+    iconSrc: '/logos/amazon.png',
   },
   {
     actionDescription: 'Give feedback often about your purchases',
     actionShows: 'You\'re interested in the benefit of others',
-    iconSrc: '/amazon.png',
+    iconSrc: '/logos/amazon.png',
   }
 ];
 
