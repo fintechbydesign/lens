bba7a9e9efa0bb2d6dbf1aa5c01f5f8dbfc53b01
Finn Ickler <finnickler@gmail.com>
2018-06-28 23:56:55 +0100

    Added css to the speech bubble component (actual styling will happen when I work on the right screen tomorrow)

--- client/src/components/Speechbubble.js
+++ client/src/components/Speechbubble.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import "./Speechbubble.css"
 // This functional component takes two props (I should just be using proptypes but
 // I can't be bothered to install it for this isolated project)
 // 1. An array of lines of text
 // 2. an boolean if its the top (true) or center speechbubble (false)
 
 function SpeechBubble(props) {
-    return <div className="speechBubble">
+    return <div className="">
                 <img src={props.top ? "/boxes/topchatbox.svg" : "/boxes/centerchatbox.svg"}/>
-                <div className="speechbubble_text">
+                <div className="speechBubble_text">
                     {props.text.map(text => <p>{text}</p>)}
                 </div>
             </div>
