e7fa35542c6b45892c413063237166dd4230faa8
Finn Ickler <finnickler@gmail.com>
2018-06-28 22:46:15 +0100

    Added the correct content to the first screen, which includes the lens logo

--- client/src/pages/Page0.js
+++ client/src/pages/Page0.js
@@ -3,10 +3,16 @@ import './Page0.css';
 
 function Page0 (props) {
   return (
-    <div name ='page0' className='flexFixedSize flexContainerColumn'>
-      <h1 className='Page0_text'>
-        What if your data could verify your interest or fit for a job, even if you haven't got any previous experience or verified education?
-      </h1>
+    <div name ='page0' className='fullPage flexFixedSize flexContainerColumn'>
+        <div className='Page0_text'>
+            <img src="logos/logo.png" width='100' height='40' alt='lens icon' className='logo' />
+            <p>
+                Hello Human!<br/>
+                Do you know what personal data makes up your digital identity? Want to use personal data to find a job?
+                Click next
+            </p>
+        </div>
+
     </div>
   );
 }
