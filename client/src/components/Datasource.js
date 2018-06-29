f145fd1e9e137e8438c33e79683cef1a16fa55f8
FinnIckler <finnickler@gmail.com>
2018-06-29 22:14:15 +0100

    Started styling the datasource component, can't get rid of some nasty margins at the moment, need to investigate on Monday/Tuesday

--- client/src/components/Datasource.js
+++ client/src/components/Datasource.js
@@ -10,19 +10,20 @@ function Datasource (props) {
     null;
   return (
     <div className={classes}>
-      <div className='flexDynamicSize'>
-        <div className="source_name heading">
-            {props.actionDescription} <img src={props.iconSrc} height='57.5px' alt='datasource icon' className='Datasource_image' />
+        <div className={props.selected ? "data_button_selected" : "data_button"}><p>&#60;</p></div>
+        <div className="flexDynamicSize Datasource_text">
+            <div className="source_name heading">
+                {props.actionDescription} <img src={props.iconSrc} height='51px' alt='datasource icon' className='Datasource_image' />
+            </div>
+            <div className="source_options text">
+                <ul>
+                    <li>{props.options['1']}</li>
+                    <li>{props.options['2']}</li>
+                    <li className="last">{props.options['3']}</li>
+                </ul>
+            </div>
+
         </div>
-        <div className="source_options text">
-            <ul>
-                <li>{props.options['1']}</li>
-                <li>{props.options['2']}</li>
-                <li className="last">{props.options['3']}</li>
-            </ul>
-        </div>
-      </div>
-      {selected}
     </div>
   );
 }
