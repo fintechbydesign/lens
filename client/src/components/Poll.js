765e16aadf0afdb2a2170a9b3c73a6dbbb48217b
FinnIckler <finnickler@gmail.com>
2018-07-03 13:04:18 +0100

    Styled the Poll component

--- client/src/components/Poll.js
+++ client/src/components/Poll.js
@@ -5,11 +5,9 @@ function Poll (props) {
   const classes = props.selected ?
       'flexContainerRow Poll_container Poll_selected' :
       'flexContainerRow Poll_container';
-  const selected = props.selected ?
-      (<img src='/Check_mark.svg' alt='Poll selected' width='75' height='30'/>) :
-      null;
   return (
       <div className={classes}>
+      <div className={props.selected ? "poll_button_selected" : "poll_button"}><p>&#60;</p></div>
         <div className='flexDynamicSize'>
           <div className="heading poll_choice">
             {props.pollChoice}
@@ -18,7 +16,6 @@ function Poll (props) {
             {props.pollDescription.map(text => <p>{text}</p>)}
           </div>
         </div>
-        {selected}
       </div>
   );
 }
