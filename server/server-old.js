6a0c6fbe4a37b18497a61511d8fca1f6874390f7
Mike Evans <mike.x.evans@tescobank.com>
2018-07-30 18:13:10 +0100

    restart serial connection spike 2

---
+++ server/server-old.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const http = require('http');
+const io = require('socket.io');
+const readLine = require('readline');
+const path = require('path');
+const serialPort = require("serialport");
+
+const config = {
+  port: 1971
+};
+
+const socketsOptions = {
+  serveClient: false
+};
+
+// set up http and ws server
+const app = express();
+const httpServer = http.createServer(app);
+const socketsServer = io(httpServer, socketsOptions);
+
+// serve client files
+const webRoot = path.resolve(__dirname, '../client/build');
+app.use("/", express.static(webRoot));
+
+// set up for console input
+// const stream = process.stdin;
+// readLine.emitKeypressEvents(stream);
+// if (stream.isTTY) {
+//   stream.setRawMode(true);
+// }
+// stream.on('keypress', (str, key) => {
+//   const { ctrl, name } = key;
+//   if (ctrl && name === 'c') {
+//     console.log('Closing server');
+//     process.exit();
+//   } else {
+//     console.log(`keypress: ${name}`);
+//     socketsServer.emit('buttonPress', {key: name});
+//   }
+// });
+//initialize serial connection with a single byte parser
+const ByteLength = serialPort.parsers.ByteLength;
+let serialConnection = new serialPort(process.argv[2], {
+  baudRate: 9600
+});
+const parser = serialConnection.pipe(new ByteLength({length: 8}));
+
+serialConnection.open(function () {
+  console.log('open');
+
+  serialConnection.on('data', function(data) {
+    let buff = new Buffer(data);
+    console.log('data received: ' + buff.toString('utf8'));
+    socketsServer.emit('buttonPress', {key: buff.toString('utf8')});
+  });
+});
+
+
+
+//error handling
+serialConnection.on("error", function () {
+  console.error("Can't establish serial connection with " + process.argv[2]);
+  process.exit(1);
+});
+serialConnection.on("close", function () {
+  console.error("Serial connection closed");
+  process.exit(1);
+});
+
+// start server
+httpServer.listen(config.port, () => {
+  console.log('\x1Bc');
+  console.log(`Server listening on port ${config.port}`);
+});
+
+
+
--- server/server.js
+++ server/server.js
@@ -13,6 +13,34 @@ const socketsOptions = {
   serveClient: false
 };
 
+const reconnectSerialPort = (socketsServer) => {
+  console.error("Serial connection hang up - reconnecting...");
+  initSerialPort(socketsServer);
+};
+
+// initialize serial connection with a single byte parser
+const initSerialPort = (socketsServer ) => {
+  const device = process.argv[2];
+  const serialConnection = new serialPort(device, {
+    baudRate: 9600
+  });
+  // const parser = serialConnection.pipe(new serialPort.parsers.ByteLength({length: 8}));
+
+  serialConnection.open(function () {
+    console.log('open');
+    serialConnection.on('data', (data) => {
+      const key = new Buffer(data).toString('utf8');
+      console.log('data received: ' + key);
+      socketsServer.emit('buttonPress', {key});
+    });
+  });
+  serialConnection.on("close", reconnectSerialPort.bind(null, socketsServer));
+  serialConnection.on("error", () => {
+    console.error("Can't establish serial connection with " + process.argv[2]);
+    process.exit(1);
+  });
+};
+
 // set up http and ws server
 const app = express();
 const httpServer = http.createServer(app);
@@ -22,56 +50,15 @@ const socketsServer = io(httpServer, socketsOptions);
 const webRoot = path.resolve(__dirname, '../client/build');
 app.use("/", express.static(webRoot));
 
-// set up for console input
-// const stream = process.stdin;
-// readLine.emitKeypressEvents(stream);
-// if (stream.isTTY) {
-//   stream.setRawMode(true);
-// }
-// stream.on('keypress', (str, key) => {
-//   const { ctrl, name } = key;
-//   if (ctrl && name === 'c') {
-//     console.log('Closing server');
-//     process.exit();
-//   } else {
-//     console.log(`keypress: ${name}`);
-//     socketsServer.emit('buttonPress', {key: name});
-//   }
-// });
-//initialize serial connection with a single byte parser
-const ByteLength = serialPort.parsers.ByteLength;
-let serialConnection = new serialPort(process.argv[2], {
-  baudRate: 9600
-});
-const parser = serialConnection.pipe(new ByteLength({length: 8}));
-
-serialConnection.open(function () {
-  console.log('open');
-
-  serialConnection.on('data', function(data) {
-    let buff = new Buffer(data);
-    console.log('data received: ' + buff.toString('utf8'));
-    socketsServer.emit('buttonPress', {key: buff.toString('utf8')});
-  });
-});
-
-
-
-//error handling
-serialConnection.on("error", function () {
-  console.error("Can't establish serial connection with " + process.argv[2]);
-  process.exit(1);
-});
-serialConnection.on("close", function () {
-  console.error("Serial connection closed");
-  process.exit(1);
-});
+// start serial port listening
+initSerialPort(socketsServer);
 
 // start server
 httpServer.listen(config.port, () => {
   console.log('\x1Bc');
   console.log(`Server listening on port ${config.port}`);
 });
+
 
 
 
